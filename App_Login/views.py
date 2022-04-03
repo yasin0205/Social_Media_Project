c0f74a2a695e5ecaa087abcfd0d3681544f0ef5b
yasin0205 <yasinislamhimaloyodesk@gmail.com.com>
2022-04-03 15:02:18 +0600

    login functionality created

--- App_Login/views.py
+++ App_Login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 
 from App_Login.forms import CreateNewUser
+from django.contrib.auth.forms import AuthenticationForm
 
 from django.contrib.auth import authenticate, login, logout
 
@@ -17,5 +18,18 @@ def sign_up(request):
             user = form.save()
             registered = True
             user_profile = UserProfile(user=user)
-            pass
+            return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request, 'App_Login/signup.html', context={'title': 'Signup page', 'form': form})
+
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                pass
+    return render(request, 'App_Login/login.html', context={'title': 'Login', 'form':form})
